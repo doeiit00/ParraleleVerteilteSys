796e84e38fc283a3266335e46c744ce5af7be108
T <149052166+tamoit02@users.noreply.github.com>
2025-04-14 07:37:43 +0000

    Erstellung erster Components und Routing

--- ShoppingList/src/app/app.routes.ts
+++ ShoppingList/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'ShoppingList', component: ShoppingListComponent},
+    { path: '', redirectTo: '/ShoppingList', pathMatch: 'full' }, // redirect to `ShoppingListComponent`
+    { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
