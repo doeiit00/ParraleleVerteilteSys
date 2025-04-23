cf12a0c950ead5f9a69025040d0c62cba43335ce
T <149052166+tamoit02@users.noreply.github.com>
2025-04-23 14:06:38 +0000

    No Docker

--- ShoppingList/src/app/app.config.ts
+++ ShoppingList/src/app/app.config.ts
@@ -1,8 +1,8 @@
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+ 
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideHttpClient(withInterceptors([])),
-  ],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
