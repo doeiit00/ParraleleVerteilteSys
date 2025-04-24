e739dd6db0ca35783be7ea2e09f7efab9ce7b0f5
T <149052166+tamoit02@users.noreply.github.com>
2025-04-24 16:31:48 +0000

    Routes and Config angepasst

--- ShoppingList/src/app/app.config.ts
+++ ShoppingList/src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
 import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withInterceptors([])),
-  ],
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([]))
+  ]
 };
