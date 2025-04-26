c46cf826cb6ef526915ed451ddfa9db47f801905
T <149052166+tamoit02@users.noreply.github.com>
2025-04-26 10:15:54 +0000

    Update PopUp

---
+++ ShoppingList/src/app/components/update-item-popup/update-item-popup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-item-popup',
+  imports: [],
+  templateUrl: './update-item-popup.component.html',
+  styleUrl: './update-item-popup.component.css'
+})
+export class UpdateItemPopupComponent {
+
+}
