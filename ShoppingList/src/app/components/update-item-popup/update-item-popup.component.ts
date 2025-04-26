b32a8502f7d9558cfba84487ae1f0c9abf3089b0
T <149052166+tamoit02@users.noreply.github.com>
2025-04-26 14:33:47 +0000

    Update works

--- ShoppingList/src/app/components/update-item-popup/update-item-popup.component.ts
+++ ShoppingList/src/app/components/update-item-popup/update-item-popup.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { Item } from '../../services/shopping-item-service.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-update-item-popup',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './update-item-popup.component.html',
   styleUrl: './update-item-popup.component.css'
 })
 export class UpdateItemPopupComponent {
+  @Input() item!: Item | undefined;
+  @Output() itemUpdated = new EventEmitter<Item>();
+  @Output() PopUpClosed = new EventEmitter<void>();
 
+  saveItem(){
+    this.itemUpdated.emit(this.item);
+  }
+
+  closePopUp(): void {
+    this.PopUpClosed.emit();
+  }
 }
